ff48f80b3596b5ecfc215a33581e790dea5f6f3a
Digital Technologia <dev.ar.arif@gmail.com>
2022-09-19 07:14:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <h1>My iClinic Updated</h1>
 <h3>Pipeline completed for feature environment</h3>
-<h3>Feature branch clear pipeline is complete</h3>
+<h3>Its feature branch & ticket 12 Updated</h3>
 
